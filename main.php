b202e21e7118a5c58d48291d20dfd85dcb9cd14b
AlizeDeMatas <32222634+AlizeDeMatas@users.noreply.github.com>
2021-03-08 23:21:37 -0500

    Add files via upload

---
+++ main.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel='icon' href='./assets/images/logo.png' >
+
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <!-- Link style.css file -->
+    <link rel="stylesheet" type="text/css" href="main.css">
+
+      <!-- Link skeleton html file -->
+    <?php include './pages/header/skeleton.html';?>
+    <link rel="stylesheet" type="text/css" href="./pages/header/header.css">
+
+
+    <!-- Google Font-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+    <title>Plan for Smart Services</title>
+  </head>
+  <body>
+  
+  <h1>Plan For Smart Services</h1>
+  <h2>Please Select an Option</h2>
+<div class=btnOpts>
+  <button class="optionA"> Ride to a Destination</button>
+  <button class="optionB"> Ride & Deliver</button>
+</div>
+<br>
+
+<footer>
+    <nav class="navbar fixed-bottom navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"></a>
+        </div>
+      </nav>
+</footer>
+  </body>
+</html>
