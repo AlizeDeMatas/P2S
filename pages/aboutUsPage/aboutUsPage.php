8aa34a1cc5987f5291aca40f7e26419aa00e4240
AlizeDeMatas <32222634+AlizeDeMatas@users.noreply.github.com>
2021-03-08 22:49:07 -0500

    Add files via upload
    aboutUsPage done

---
+++ pages/aboutUsPage/aboutUsPage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Link style.css file -->
+    <link rel="stylesheet" type="text/css" href="./aboutUsPage.css">
+    
+     <!-- Google Font-->
+     <link rel="preconnect" href="https://fonts.gstatic.com">
+     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+    
+       <!-- Link skeleton html file -->
+    <?php include '../header/skeleton.html';?>
+    <link rel="stylesheet" type="text/css" href="../header/header.css">
+
+
+    <title>About Us</title>
+</head>
+<body>
+    <h1>About Us</h1>
+
+    <ul class= "pics">
+    
+    <li><div><img src="../../assets/aboutUsImages/alize.png" alt="alize pic" id="alizePic"></div><br>
+    <h2>Alize De Matas</h2>
+    <p> Hello! I am a fifth year Computer Science student</p><br></a>
+    <li><div><img src="../../assets/aboutUsImages/homayra.png" alt="hom pic" id="homPic"></div><br></a>
+    <h2>Homayra Mussarrat</h2>
+    <p> Hello! I am a third year Computer Science student</p><br></a>
+    <li><div><img src="../../assets/aboutUsImages/saad.png" alt="saad pic" id="saadPic"></div><br></a>
+    <h2>Saad Iqbal</h2>
+    <p> Hello! I am a fifth year Computer Science student</p><br></a>
+</ul>
+
+</body>
+</html>
